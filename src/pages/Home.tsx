8ca882228f62af4332d83c5980feb54f256142c9
crystallographer <r0475582@proton.me>
2023-04-01 14:45:09 +0400

    feat: header component added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,10 @@
-import logo from "../../assets/images/logo.svg";
+import { Header } from "../components/Header";
 import { Main } from "../layouts/Main";
 
 export const Home = function () {
     return (
         <Main>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Header />
         </Main>
     );
 };
