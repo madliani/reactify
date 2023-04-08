d6cd0657660b73d2699d8744067b704693a10c7e
crystallographer <r0475582@proton.me>
2023-04-08 22:19:36 +0400

    feat: react-helmet added

---
+++ src/components/SEO.tsx
@@ -0,0 +1,56 @@
+import { Helmet } from "react-helmet";
+
+type Meta = { content: string; name: string };
+
+type Properties = {
+    description?: string;
+    lang?: string;
+    meta?: Meta[];
+    title: string;
+};
+
+export const SEO = function ({
+    description = "The React Starter Kit.",
+    lang = "en",
+    meta = [],
+    title
+}: Properties) {
+    // eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
+    const htmlAttributes = {
+        lang
+    };
+    const titleTemplates = "Reactify | %s";
+
+    return (
+        <Helmet
+            htmlAttributes={htmlAttributes}
+            // eslint-disable-next-line react-perf/jsx-no-new-array-as-prop
+            meta={[
+                {
+                    content: description,
+                    name: "description"
+                },
+                {
+                    content: description,
+                    property: "og:description"
+                },
+                {
+                    content:
+                        "https://raw.githubusercontent.com/crystallographer/reactify/main/src/assets/images/logo.svg",
+                    name: "og:image"
+                },
+                {
+                    content: "github.com/reactify",
+                    name: "og:site_name"
+                },
+                {
+                    content: title,
+                    property: "og:title"
+                },
+                ...meta
+            ]}
+            title={title}
+            titleTemplate={titleTemplates}
+        />
+    );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import { Header } from "../components/Header";
+import { SEO } from "../components/SEO";
 import { Main } from "../layouts/Main";
 
 export const Home = function () {
     return (
-        <Main>
-            <Header />
-        </Main>
+        <>
+            <SEO title="Home" />
+            <Main>
+                <Header />
+            </Main>
+        </>
     );
 };
