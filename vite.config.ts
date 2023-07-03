c5ae28ee7ca3b629acfff8d7e75e3b11868badbc
crystallographer <r0475582@proton.me>
2023-07-03 13:32:04 +0400

    feat: parcel deleted and vite added

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import EnvironmentPlugin from "vite-plugin-environment";
+
+export default defineConfig({
+    esbuild: {
+        jsxFactory: "h",
+        jsxFragment: "Fragment",
+        jsxInject: "import React from 'react'"
+    },
+    plugins: [EnvironmentPlugin("all"), react()]
+});
