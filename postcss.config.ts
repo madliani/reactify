6ca1815d3b1d8f3351173c8b5d30b0d1e9f17edb
madliani <madliani@hotmail.com>
2024-03-12 18:41:08 +0400

    feat: autoprefixer added

---
+++ postcss.config.ts
@@ -0,0 +1,3 @@
+export default {
+    plugins: ["autoprefixer", "postcss-preset-env"]
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import react from "@vitejs/plugin-react-swc";
+import autoprefixer from "autoprefixer";
+import postCssPresetEnv from "postcss-preset-env";
 import { defineConfig } from "vite";
 
 export default defineConfig({
     base: "/",
+    css: {
+        postcss: {
+            plugins: [autoprefixer, postCssPresetEnv]
+        }
+    },
     plugins: [react()],
     server: {
         cors: true,
