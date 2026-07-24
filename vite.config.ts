56f55bd9adf1affab70515ed5e5dc61093cbb3ee
madliani <unixsocket@hotmail.com>
2026-07-24 17:42:32 +0400

    refactor: refactor Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import react from "@vitejs/plugin-react-swc";
+import pluginReactSWC from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import { compression } from "vite-plugin-compression2";
-import svgr from "vite-plugin-svgr";
+import { compression as pluginCompression } from "vite-plugin-compression2";
+import pluginSVGR from "vite-plugin-svgr";
 
 export default defineConfig({
     plugins: [
-        compression({
+        pluginCompression({
             algorithm: "brotliCompress",
             exclude: [/\.(br)$/, /\.(gz)$/]
         }),
-        react(),
-        svgr()
+        pluginReactSWC(),
+        pluginSVGR()
     ],
     server: {
         cors: true,
