0d32f43e4e6eab884188774c1c0f011ad9e25b78
madliani <unixsocket@hotmail.com>
2026-07-25 19:47:23 +0400

    refactor: refactor Main component

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -4,8 +4,8 @@ type MainProps = Readonly<{
     children: ReactNode;
 }>;
 
-const Main = ({ children }: MainProps) => {
-    return <div className="container">{children}</div>;
-};
+const Main = ({ children }: MainProps) => (
+    <div className="container">{children}</div>
+);
 
 export default Main;
