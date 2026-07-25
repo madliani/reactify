4fdef3b696ebbedb380298b37dfc7568a617982d
madliani <unixsocket@hotmail.com>
2026-07-25 19:51:11 +0400

    refactor: refactor Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,9 @@ import Counter from "../components/Counter";
 import SEO from "../components/SEO";
 import Main from "../layouts/Main";
 
-export const Home = () => {
-    return (
-        <Main>
-            <SEO title="Home" />
-            <Counter />
-        </Main>
-    );
-};
+export const Home = () => (
+    <Main>
+        <SEO title="Home" />
+        <Counter />
+    </Main>
+);
