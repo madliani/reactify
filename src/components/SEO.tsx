ef2aa8ace33b6421d632a02d8e491517a0b658c2
madliani <unixsocket@hotmail.com>
2026-07-25 19:35:36 +0400

    refactor: refactor SEO component

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -11,17 +11,15 @@ const titleTemplate = (title: string) => `Reactify | ${title}`;
 export const SEO = ({
     description = "The React Starter Kit.",
     title
-}: Properties) => {
-    return (
-        <Helmet title={titleTemplate(title)} prioritizeSeoTags>
-            <meta content={description} name="description" />
-            <meta content={description} name="og:description" />
-            <meta
-                content="https://raw.githubusercontent.com/madliani/reactify/main/assets/images/reactify-homepage.png"
-                name="og:image"
-            />
-            <meta content="github.com/reactify" name="og:site_name" />
-            <meta content={titleTemplate(title)} name="og:title" />
-        </Helmet>
-    );
-};
+}: Properties) => (
+    <Helmet title={titleTemplate(title)} prioritizeSeoTags>
+        <meta content={description} name="description" />
+        <meta content={description} name="og:description" />
+        <meta
+            content="https://raw.githubusercontent.com/madliani/reactify/main/assets/images/reactify-homepage.png"
+            name="og:image"
+        />
+        <meta content="github.com/reactify" name="og:site_name" />
+        <meta content={titleTemplate(title)} name="og:title" />
+    </Helmet>
+);
