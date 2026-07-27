a0ad6472203685195e54965d5a412af3465e1a73
madliani <unixsocket@hotmail.com>
2026-07-27 15:49:58 +0400

    refactor: refactor App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,8 @@ import { HelmetProvider } from "react-helmet-async";
 import AppRouter from "../router/AppRouter";
 import "../styles/index.css";
 
-export const App = () => {
-    return (
-        <HelmetProvider>
-            <AppRouter />
-        </HelmetProvider>
-    );
-};
+export const App = () => (
+    <HelmetProvider>
+        <AppRouter />
+    </HelmetProvider>
+);
