316b21d7f5ee99e29329315aebb7036867dfbe73
madliani <unixsocket@hotmail.com>
2026-09-09 16:22:28 +0400

    fix: fix ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -12,7 +12,7 @@ import globals from "globals";
 import { configs as tsConfigs, parser as tsParser } from "typescript-eslint";
 
 export default defineConfig([
-    globalIgnores(["node_modules/"]),
+    globalIgnores([".vitest/", "coverage/", "dist/", "node_modules/"]),
     {
         extends: [
             js.configs.recommended,
