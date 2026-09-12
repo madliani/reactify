01c452b66fe1b8e3dfad0489860b052b28ef143c
madliani <unixsocket@hotmail.com>
2026-09-12 16:12:45 +0400

    fix: make ESLint config more simple

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,7 @@
 import js from "@eslint/js";
 import pluginCompat from "eslint-plugin-compat";
 import pluginImport from "eslint-plugin-import";
-import pluginJSXA11Y from "eslint-plugin-jsx-a11y";
 import pluginNoUnsanitized from "eslint-plugin-no-unsanitized";
-import pluginPromise from "eslint-plugin-promise";
-import { configs as regExpConfigs } from "eslint-plugin-regexp";
-import pluginSonarJS from "eslint-plugin-sonarjs";
 import pluginSort from "eslint-plugin-sort";
 import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
@@ -27,11 +23,7 @@ export default defineConfig([
             pluginCompat.configs["flat/recommended"],
             pluginImport.flatConfigs.recommended,
             pluginImport.flatConfigs.typescript,
-            pluginJSXA11Y.flatConfigs.recommended,
             pluginNoUnsanitized.configs.recommended,
-            pluginPromise.configs["flat/recommended"],
-            regExpConfigs["flat/recommended"],
-            pluginSonarJS.configs.recommended,
             pluginSort.configs["flat/recommended"]
         ],
 
