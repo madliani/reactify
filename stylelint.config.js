469b26d15fa272bde8a1995d3289c55f67b70acd
madliani <unixsocket@hotmail.com>
2026-09-20 17:15:18 +0400

    fix: replace Stylelint config for SCSS by Stylelint config for CSS

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,7 +3,7 @@ export default {
     allowEmptyInput: true,
     cache: true,
     defaultSeverity: "error",
-    extends: ["stylelint-config-standard-scss", "stylelint-config-clean-order"],
+    extends: ["stylelint-config-standard", "stylelint-config-clean-order"],
     fix: true,
     maxWarnings: 0,
     plugins: ["stylelint-no-unsupported-browser-features"],
