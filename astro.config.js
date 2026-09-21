7bde296130cf12799ca8a3ee5b24dd809d3e6edf
madliani <unixsocket@hotmail.com>
2026-09-21 18:46:57 +0400

    fix: fix Astro config

--- astro.config.js
+++ astro.config.js
@@ -2,6 +2,7 @@ import reactIntegration from "@astrojs/react"
 import sitemapIntegration from "@astrojs/sitemap"
 import vitePluginTW from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
+import path from "node:path"
 
 const PORT = 4321
 
@@ -26,5 +27,13 @@ export default defineConfig({
     },
     site: "https://madliani-reactify.vercel.app/",
 
-    vite: { plugins: [...vitePlugins] }
+    vite: {
+        plugins: [...vitePlugins],
+        resolve: {
+            alias: {
+                "@react": path.resolve(import.meta.dirname, "./src/react/"),
+                "@src": path.resolve(import.meta.dirname, "./src/")
+            }
+        }
+    }
 })
