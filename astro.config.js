f9c17857f96db18c47348cf024e922ad806442ad
madliani <unixsocket@hotmail.com>
2026-09-22 18:38:31 +0400

    fix: delete Tailwind CSS

--- astro.config.js
+++ astro.config.js
@@ -1,13 +1,11 @@
 import reactIntegration from "@astrojs/react"
 import sitemapIntegration from "@astrojs/sitemap"
-import vitePluginTW from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import path from "node:path"
 
 const PORT = 4321
 
 const integrations = [reactIntegration(), sitemapIntegration()]
-const vitePlugins = [vitePluginTW()]
 
 /** @type {import("astro").AstroConfig} */
 export default defineConfig({
@@ -28,7 +26,6 @@ export default defineConfig({
     site: "https://madliani-reactify.vercel.app/",
 
     vite: {
-        plugins: [...vitePlugins],
         resolve: {
             alias: {
                 "@react": path.resolve(import.meta.dirname, "./src/react/"),
